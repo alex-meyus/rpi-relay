d588162711a2d193f4277deedf6e591f6675cfa1
alex-meyus <115928293+alex-meyus@users.noreply.github.com>
2025-01-15 22:51:32 +0100

    Update install.sh
    crontab added

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ apt update
 apt install mosquitto-clients
 chmod +x gpio_mqtt_control.sh
 echo "Check Config of MQTT and GPIO"
+(crontab -l 2>/dev/null; echo "@reboot sleep 5 && /home/alex/rpi-relay/gpio_mqtt_control.sh &") | crontab -
