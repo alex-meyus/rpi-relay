68d0b2555da6876d26f633c5bfe863217edf2c62
alex-meyus <alex.meyer.pub@gmx.de>
2025-01-15 22:05:28 +0100

    add gpio.sh

---
+++ gpio_mqtt_control.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#https://www.waveshare.com/wiki/RPi_Relay_Board_(B)
+#Channel number	RPi pin number	wiringPi	BCM	Description
+#Channel label	29	P21	5	Channel 1
+#Channel label	31	P22	6	Channel 2
+#Channel label	33	P23	13	Channel 3
+#Channel label	36	P27	16	Channel 4
+#Channel label	35	P24	19	Channel 5
+#Channel label	38	P28	20	Channel 6
+#Channel label	40	P29	21	Channel 7
+#Channel label	37	P25	26	Channel 8
+
+# MQTT-Einstellungen
+MQTT_SERVER="srv-iobroker"
+MQTT_TOPIC="gpio/#"
+MQTT_USER="mqtt"
+MQTT_PW="mqtt"
+MQTT_PORT="1884"
+
+# MQTT-Nachrichten verarbeiten
+process_message() {
+    topic=$1
+    echo "pm1:"$1
+    message=$2
+    echo "pm2:"$2
+    
+    # GPIO-Nummer aus dem Topic extrahieren
+    gpio=$(echo $topic | cut -d'/' -f2)
+    echo "pm3:"$gpio
+    # Befehl aus der Nachricht extrahieren
+    command=$message
+    
+    case $command in
+        "true") #inverse Logik!
+            # GPIO einschalten
+            #echo "1" > /sys/class/gpio/gpio$gpio/value
+            pinctrl set $gpio op dl
+            ;;
+        "false")
+            # GPIO ausschalten
+            #echo "0" > /sys/class/gpio/gpio$gpio/value
+            pinctrl set $gpio op dh
+            ;;
+        *)
+            echo "Unbekannter Befehl: $command"
+            ;;
+    esac
+}
+
+# MQTT-Subscription starten
+mosquitto_sub -h $MQTT_SERVER -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PW -t $MQTT_TOPIC -v | while read -r line
+do
+    echo "Read from mqtt: >>"$line"<<"
+    topic=$(echo $line | cut -d' ' -f1)
+    #topic=$(echo $line | cut -d' ' -f1 | cut -d'/' -f2)
+    echo $topic
+    message=$(echo $line | cut -d' ' -f2-)
+    echo $message
+    process_message "$topic" "$message"
+done
