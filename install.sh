56dffc0aaf34a0dc93df031f3802e534a5dd2503
alex-meyus <115928293+alex-meyus@users.noreply.github.com>
2025-01-15 23:12:39 +0100

    Update install.sh
    daemon added

--- install.sh
+++ install.sh
@@ -2,5 +2,11 @@
 apt update
 apt install mosquitto-clients
 chmod +x gpio_mqtt_control.sh
+
 echo "Check Config of MQTT and GPIO"
-(crontab -l 2>/dev/null; echo "@reboot sleep 5 && /home/alex/rpi-relay/gpio_mqtt_control.sh &") | crontab -
+
+#Autostart --> Daemon
+# funktioniert nicht #(crontab -l 2>/dev/null; echo "@reboot sleep 5 && /home/alex/rpi-relay/gpio_mqtt_control.sh &") | crontab -
+mv rpi-relay.service /etc/systemd/system/rpi-relay.service
+systemctl enable rpi-relay.service
+systemctl start rpi-relay.service
